670bf54a8e96ea6ebbb846e24241cb04923d62cb
MariaRuizGutierrez <marikichi1996@gmail.com>
2017-11-03 12:26:30 +0100

    Quitado paquete sample y añadido paquete para los test.

--- Proyecto Java/Acme-Explorer/src/main/java/services/AdministratorService.java
+++ Proyecto Java/Acme-Explorer/src/main/java/services/AdministratorService.java
@@ -1,11 +1,15 @@
 
 package services;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.AdministratorRepository;
+import domain.Administrator;
 
 @Service
 @Transactional
@@ -26,4 +30,36 @@ public class AdministratorService {
 
 	// Simple CRUD methods------------------------------------------------
 
+	public Administrator create() {
+		Administrator result;
+		result = new Administrator();
+		return result;
+	}
+
+	public Collection<Administrator> findAll() {
+		Collection<Administrator> result;
+		result = this.administratorRepository.findAll();
+		Assert.notNull(result);
+		return result;
+	}
+
+	public Administrator findOne(int administratorId) {
+		Administrator result;
+		result = this.administratorRepository.findOne(administratorId);
+		Assert.notNull(result);
+		return result;
+	}
+
+	public Administrator save(Administrator administrator) {
+		Assert.notNull(administrator);
+		Administrator result;
+		result = this.administratorRepository.save(administrator);
+		return result;
+	}
+
+	public void delete(Administrator administrator) {
+		Assert.notNull(administrator);
+		Assert.isTrue(administrator.getId() != 0);
+		this.administratorRepository.delete(administrator);
+	}
 }
