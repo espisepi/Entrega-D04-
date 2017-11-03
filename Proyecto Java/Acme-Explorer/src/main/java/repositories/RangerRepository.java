b74f84c7c18bb0812dc1222b17c4bccdfdd6df8d
lauverrec <lauraverarec@gmail.com>
2017-11-03 20:01:16 +0100

    Mis repositorios

---
+++ Proyecto Java/Acme-Explorer/src/main/java/repositories/RangerRepository.java
@@ -0,0 +1,12 @@
+
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.Ranger;
+
+@Repository
+public interface RangerRepository extends JpaRepository<Ranger, Integer> {
+
+}
