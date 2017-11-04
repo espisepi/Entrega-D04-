be3fd185f6ddf211f622b555efd18034b893ca92
joaquinperez94 <blogdeaula78@gmail.com>
2017-11-04 10:55:31 +0100

    Cambios

--- Proyecto Java/Acme-Explorer/src/main/java/services/TagService.java
+++ Proyecto Java/Acme-Explorer/src/main/java/services/TagService.java
@@ -1,16 +1,22 @@
 
 package services;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.TagRepository;
+import domain.Tag;
 
 @Service
 @Transactional
 public class TagService {
 
 	// Managed repository -----------------------------------------------------
+	@Autowired
 	private TagRepository	tagRepository;
 
 
@@ -21,4 +27,43 @@ public class TagService {
 		super();
 	}
 
+	// Simple CRUD methods-----------------------------------------------------
+	public Tag create() {
+		Tag result;
+		result = new Tag();
+		return result;
+	}
+
+	public Collection<Tag> findAll() {
+		Collection<Tag> result;
+		Assert.notNull(this.tagRepository);
+		result = this.tagRepository.findAll();
+		Assert.notNull(result);
+		return result;
+	}
+
+	public Tag findOne(final int tagId) {
+		Tag result;
+		result = this.tagRepository.findOne(tagId);
+		return result;
+	}
+
+	public Tag save(final Tag tag) {
+		assert tag != null;
+
+		Tag result;
+
+		result = this.tagRepository.save(tag);
+
+		return result;
+	}
+
+	public void delete(final Tag tag) {
+		assert tag != null;
+		assert tag.getId() != 0;
+
+		Assert.isTrue(this.tagRepository.exists(tag.getId()));
+
+		this.tagRepository.delete(tag);
+	}
 }
