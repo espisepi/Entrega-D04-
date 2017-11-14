448def8f25e450044a25680821e540469a66f35f
espisepi <sepinaco@gmail.com>
2017-11-14 16:16:36 +0100

    Tocado el populate

--- Proyecto Java/Acme-Explorer/src/test/java/services/CategoryServicetest.java
+++ Proyecto Java/Acme-Explorer/src/test/java/services/CategoryServicetest.java
@@ -1,15 +1,13 @@
 
 package services;
 
-import java.util.Collection;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 
 import utilities.AbstractTest;
 import domain.Category;
@@ -28,53 +26,56 @@ public class CategoryServicetest extends AbstractTest {
 
 	// Supporting services ----------------------------------------------------
 
+	//	@Test
+	//	public void testCreate() {
+	//		super.authenticate("administrator1");
+	//		Category category;
+	//
+	//		category = this.categoryService.create();
+	//		Assert.notNull(category.getSubCategories());
+	//
+	//		super.unauthenticate();
+	//	}
+	//	@Test
+	//	public void testSave() {
+	//		super.authenticate("administrator1");
+	//		Category result;
+	//
+	//		result = this.categoryService.create();
+	//		result.setName("Name test");
+	//		result.setFatherCategory(this.categoryService.findOne(super.getEntityId("climbing")));
+	//		result = this.categoryService.save(result);
+	//		Assert.isTrue(result.getId() != 0);
+	//
+	//		super.unauthenticate();
+	//
+	//	}
+
 	@Test
-	public void testCreate() {
-		super.authenticate("administrator1");
-		Category category;
-
-		category = this.categoryService.create();
-		Assert.notNull(category.getSubCategories());
-
-		super.unauthenticate();
-	}
-	@Test
-	public void testSave() {
-		super.authenticate("administrator1");
-		Category result;
-
-		result = this.categoryService.create();
-		result.setName("Name test");
-		result.setFatherCategory(this.categoryService.findOne(super.getEntityId("climbing")));
-		result = this.categoryService.save(result);
-		Assert.isTrue(result.getId() != 0);
-
-		super.unauthenticate();
-
-	}
-
-	@Test
+	@Rollback(false)
 	public void testDelete() {
 		super.authenticate("administrator1");
 		Category category;
 
-		category = this.categoryService.findOne(super.getEntityId("climbing"));
+		category = this.categoryService.findOne(super.getEntityId("category"));
+		//Me coge la category.SubCategories() como null?
+		System.out.println(category.getSubCategories().size());
 		this.categoryService.delete(category);
 
 		super.unauthenticate();
 	}
 
-	@Test
-	public void testFindAllPositive() {
-		Collection<Category> categorys;
-		categorys = this.categoryService.findAll();
-		Assert.notEmpty(categorys);
-	}
-
-	@Test
-	public void testFindOne() {
-		Category category;
-		category = this.categoryService.findOne(super.getEntityId("climbing"));
-		Assert.notNull(category);
-	}
+	//	@Test
+	//	public void testFindAllPositive() {
+	//		Collection<Category> categorys;
+	//		categorys = this.categoryService.findAll();
+	//		Assert.notEmpty(categorys);
+	//	}
+	//
+	//	@Test
+	//	public void testFindOne() {
+	//		Category category;
+	//		category = this.categoryService.findOne(super.getEntityId("climbing"));
+	//		Assert.notNull(category);
+	//	}
 }
